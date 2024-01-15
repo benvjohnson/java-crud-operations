82981cb20d33c68ba7877b21bbc8c423a3c9206d
benvjohnson <benjohnsonv98@gmail.com>
2024-01-16 00:07:16 +0530

    add : Student Model

---
+++ my-app/src/main/java/com/example/restservice/Model/Student.java
@@ -0,0 +1,35 @@
+package com.example.restservice.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
+
+/**
+ * Student Model
+ * @author Ben Johnson
+ */
+
+ @Entity
+ @Getter
+ @Setter
+ @AllArgsConstructor
+ @NoArgsConstructor
+
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @NaturalId(mutable= true)
+    private String email;
+    private String department;
+
+}
